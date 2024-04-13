308b07dc6c13b724c2e9942f05ba9e94d8299ef9
Roman Pidlipskiy <108235770+Randayy@users.noreply.github.com>
2024-04-13 13:35:29 +0300

    fixed version and used aioredis instead

--- app/routers/health_check_route.py
+++ app/routers/health_check_route.py
@@ -1,18 +1,18 @@
 from fastapi import APIRouter
-from app.db.connect_redis import redis_client
+from app.db.connect_redis import check_redis_connection
 from app.db.connect_postgresql import check_connection
 
 router = APIRouter()
 
 @router.get("/")
 async def health_check():
-    redis = await redis_client.get_redis()
-    redis_status = await redis.ping()
+    redis = await check_redis_connection()
+    # redis_status = await redis.ping()
     postgres = await check_connection()
     return { 
             "status_code": 200,
             "detail": "ok",
             "result": "working",
-            "redis_status": f"{redis_status}",
+            "redis_status": f"{redis}",
             "postgres_status": f"{postgres}"
             }
