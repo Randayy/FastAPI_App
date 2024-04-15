7c101fae8b0e821ceb78bba086e8c904e96a9cc7
Roman Pidlipskiy <108235770+Randayy@users.noreply.github.com>
2024-04-15 15:28:03 +0300

    fixed_finally

--- app/db/base_models.py
+++ app/db/base_models.py
@@ -4,8 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class BaseTable(Base):
-    # __abstract__ = True
-    __tablename__ = 'base_table'
+    __abstract__ = True
 
     id = Column(Integer, primary_key=True, index=True)
 
