e96bd3d0d58980a9176dfa613e5d3f696cde0127
Roman Pidlipskiy <108235770+Randayy@users.noreply.github.com>
2024-04-15 16:00:22 +0300

    email

--- app/db/user_models.py
+++ app/db/user_models.py
@@ -6,6 +6,6 @@ class User(BaseTable):
 
     username = Column(String, unique=True, index=True,nullable=False)
     password = Column(String,nullable=False)
-    email = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True,nullable=False)
     first_name = Column(String, nullable=True)
     last_name = Column(String, nullable=True)
