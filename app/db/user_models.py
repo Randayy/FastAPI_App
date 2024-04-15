09c244bc074a0fc344d4660989252faf1eebaf55
Roman Pidlipskiy <108235770+Randayy@users.noreply.github.com>
2024-04-15 15:52:24 +0300

    fix

--- app/db/user_models.py
+++ app/db/user_models.py
@@ -4,8 +4,8 @@ from app.db.base_models import BaseTable
 class User(BaseTable):
     __tablename__ = 'users'
 
-    username = Column(String, unique=True, index=True)
-    password = Column(String)
+    username = Column(String, unique=True, index=True,nullable=False)
+    password = Column(String,nullable=False)
     email = Column(String, unique=True, index=True)
     first_name = Column(String, nullable=True)
     last_name = Column(String, nullable=True)
