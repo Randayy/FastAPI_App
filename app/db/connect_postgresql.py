e5e737ead665e103c818aac8346fd1e7023ecbf1
Roman Pidlipskiy <108235770+Randayy@users.noreply.github.com>
2024-04-15 11:26:53 +0300

    fix

--- app/db/connect_postgresql.py
+++ app/db/connect_postgresql.py
@@ -9,7 +9,7 @@ from sqlalchemy import text
 
 settings = Settings()
 
-DATABASE_URL = f"postgresql+asyncpg://{settings.db_user}:{settings.db_password}@localhost:{settings.db_port}/{settings.db_name}"
+DATABASE_URL = f"postgresql+asyncpg://{settings.db_user}:{settings.db_password}@{settings.db_host}:{settings.db_port}/{settings.db_name}"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 
--- app/db/models.py
+++ app/db/models.py
@@ -3,11 +3,13 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-
-class User(Base):
-    __tablename__ = 'users'
+class BaseTable(Base):
+    __abstract__ = True
 
     id = Column(Integer, primary_key=True, index=True)
+class User(BaseTable):
+    __tablename__ = 'users'
+
     username = Column(String, unique=True, index=True)
     password = Column(String)
     email = Column(String, unique=True, index=True)
