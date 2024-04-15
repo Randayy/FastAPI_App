08d4ba6a9f7e3d4e8059de15b7fbcc655620263b
Roman Pidlipskiy <108235770+Randayy@users.noreply.github.com>
2024-04-15 15:52:43 +0300

    delete comment

--- alembic/versions/f6b91c1bac43_initial.py
+++ alembic/versions/f6b91c1bac43_initial.py
@@ -11,7 +11,6 @@ from alembic import op
 import sqlalchemy as sa
 
 
-# revision identifiers, used by Alembic.
 revision: str = 'f6b91c1bac43'
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
