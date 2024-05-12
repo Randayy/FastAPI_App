6426edbf06923e2796f296c92644367d0073d591
Roman Pidlipskiy <108235770+Randayy@users.noreply.github.com>
2024-05-12 12:54:11 +0300

    added scheduler in previus pr also is part

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-
+from apscheduler.schedulers.background import BackgroundScheduler
 from app.routers.health_check_route import health_check_router
 from app.routers.user_routes import user_router
 from app.routers.company_routes import company_router
 from app.routers.quiz_routes import quiz_router
 from app.core.config import Settings
+from app.scheduler.apscheduler import check_quiz_completion
 from fastapi.middleware.cors import CORSMiddleware
 
 settings = Settings()
@@ -28,3 +29,7 @@ if __name__ == "__main__":
     import uvicorn
     uvicorn.run("app.main:app", host=settings.host,
                 port=settings.port, reload=settings.debug)
+    
+scheduler = BackgroundScheduler()
+scheduler.add_job(check_quiz_completion, 'interval', days=1, start_date='2022-01-01 00:00:00')
+scheduler.start()
